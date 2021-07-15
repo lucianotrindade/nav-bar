aea50a509d8b003a1653590e3ccdbb7f56716dc1
Luciano <lucianoapatrindade@gmail.com>
2021-07-15 14:22:52 -0300

    inclui o arquivo script.js

---
+++ assest/js/script.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('.menu');
+const bars = document.querySelector('.bars');
+const times = document.querySelector('.times');
+
+
+function ocultaMenu (){
+    menu.style.display = 'none';   
+}
+
+function abreMenu () {
+    menu.style.display = 'flex';   
+}
+
+
+bars.addEventListener ('click', function (e) {
+    abreMenu();
+    bars.style.display = "none";
+    times.style.display = "flex";
+});
+
+times.addEventListener ('click' , function (e) {
+    ocultaMenu();
+    times.style.display = 'none';
+    bars.style.display = 'flex';
+});
